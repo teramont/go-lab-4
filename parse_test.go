bc0f64873e5917ba834aa9f057c86efc185eb666
teramont <rfibruk@gmail.com>
2020-12-06 13:44:32 +0200

    Add parser tests

---
+++ parse_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPostfixToPrefix(t *testing.T) {
+	print := ParseCommand("print", "Hello world")
+	printc := ParseCommand("printc", "12 h")
+
+	assert.Equal(t, print, &PrintCommand{Msg: "Hello world"})
+	assert.Equal(t, printc, &PrintcCommand{Count: 12, Str: "h"})
+}
